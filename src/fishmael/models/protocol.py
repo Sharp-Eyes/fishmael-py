02ba054f716884c40ccc0f22eb4b7c9be20cdeff
Chromosomologist <chromosomologist@gmail.com>
2024-10-14 12:27:41 +0200

    feat: make protocols runtime checkable

--- src/fishmael/models/protocol.py
+++ src/fishmael/models/protocol.py
@@ -3,11 +3,16 @@ import typing
 
 from fishmael.models.shard import ShardId
 
+__all__: typing.Sequence[str] = ("StreamKeyProvider", "Streamable")
+
+
+@typing.runtime_checkable
 class StreamKeyProvider(typing.Protocol):
     @classmethod
     def get_stream_key(cls, shard: ShardId, /) -> bytes: ...
 
 
+@typing.runtime_checkable
 class Streamable(StreamKeyProvider, typing.Protocol):
     @classmethod
     def from_raw(cls, data: collections.abc.Mapping[bytes, bytes], /) -> "Streamable": ...
