5b7a1af6fe19d0edf4dce0504371b57fe8b10076
Chromosomologist <chromosomologist@gmail.com>
2024-10-17 12:10:58 +0200

    feat: implement simple event exception handling

--- src/fishmael/event_manager.py
+++ src/fishmael/event_manager.py
@@ -2,6 +2,7 @@ import asyncio
 import collections.abc
 import dataclasses
 import inspect
+import logging
 import types
 import typing
 
@@ -10,21 +11,53 @@ from fishmael.internal import async_utils, class_utils
 
 __all__: collections.abc.Sequence[str] = ("EventManager",)
 
+_LOGGER: typing.Final[logging.Logger] = logging.getLogger(__name__)
+
 ListenerMap = dict[
     type[events_base.Event],
     list[events_base.EventCallbackT[events_base.Event]],
 ]
 
+
 @dataclasses.dataclass
 class EventManager:
     _listeners: ListenerMap = dataclasses.field(default_factory=dict, init=False)
+
+    async def _handle_callback(
+        self,
+        callback: events_base.EventCallbackT[events_base.Event],
+        event: events_base.Event,
+    ) -> None:
+        try:
+            await callback(event)
+
+        except Exception as exc:
+            if isinstance(event, events_base.ExceptionEvent):
+                exc_info = type(exc), exc, exc.__traceback__.tb_next if exc.__traceback__ else None
+
+                _LOGGER.exception(
+                    "An exception occurred while handling event %r in '%s.%s', and was ignored.",
+                    type(event).__name__,
+                    callback.__module__,
+                    callback.__qualname__,
+                    exc_info=exc_info,
+                )
+                return
+
+            _LOGGER.exception(
+                "An exception occurred while handling event %r in '%s.%s'.",
+                type(event).__name__,
+                callback.__module__,
+                callback.__qualname__,
+            )
+            await self.dispatch(events_base.ExceptionEvent(exc, event, callback))
 
     def dispatch(self, event: events_base.Event) -> asyncio.Future[typing.Any]:
         tasks: list[collections.abc.Coroutine[None, None, None]] = []
 
         for cls in event.dispatches:
             for callback in self._listeners.get(cls, ()):
-                tasks.append(callback(event))
+                tasks.append(self._handle_callback(callback, event))
 
             # TODO: waiters
 
@@ -68,40 +101,30 @@ class EventManager:
             event_param = next(iter(parameters))
             annotation = event_param.annotation
 
-            # No annotation provided...
             if annotation is inspect.Parameter.empty:
-                if event_types:
-                    resolved_types = event_types
-
-                else:
+                if not event_types:
                     msg = (
                         "Please provide the event type either as an argument to"
                         " the decorator or as a type hint."
                     )
                     raise TypeError(msg)
 
-            # Annotation was provided...
-            else:
-                if typing.get_origin(annotation) in (typing.Union, types.UnionType):
-                    resolved_types = {
-                        class_utils.strip_generic(ann)
-                        for ann in typing.get_args(annotation)
-                    }
-                else:
-                    resolved_types = {class_utils.strip_generic(annotation)}
+                resolved_types = event_types
 
-                # If both were provided, all decorator types must be subclasses
-                # of the annotation types for it to be typesafe.
-                for event_type in event_types:
-                    for resolved_type in resolved_types:
-                        if resolved_type not in event_type.mro():
-                            msg = (
-                                "Please make sure the event types provided to the"
-                                " decorator match those provided as a typehint."
-                            )
-                            raise TypeError(msg)
+            elif typing.get_origin(annotation) in (typing.Union, types.UnionType):
+                resolved_types = {
+                    class_utils.strip_generic(ann)
+                    for ann in typing.get_args(annotation)
+                }
+
+            else:
+                resolved_types = {class_utils.strip_generic(annotation)}
 
             for event_type in resolved_types:
+                if events_base.Event not in event_type.mro():
+                    msg = f"Expected event class, got {event_type.__name__!r}"
+                    raise TypeError(msg)
+
                 self.subscribe(event_type, callback)
 
             return callback
--- test.py
+++ test.py
@@ -1,19 +1,25 @@
 import asyncio
-import typing
+import logging
 
 import dotenv
 
 import fishmael
 
+logging.basicConfig(level=logging.INFO)
 
 async def main() -> None:
     dotenv.load_dotenv()
 
     client = await fishmael.Fishmael.from_env()
 
-    @client.listen()
+    @client.listen(fishmael.events.CommandInteractionEvent)
     async def foo(ev: fishmael.events.InteractionEvent) -> None:  # pyright: ignore[reportUnusedFunction]
         print("omg???", ev.interaction_id)  # noqa: T201
+        raise Exception
+
+    @client.listen()
+    async def bleh(ev: fishmael.events.ExceptionEvent) -> None:  # pyright: ignore[reportUnusedFunction]
+        raise ev.exception
 
     await client.start()
 
